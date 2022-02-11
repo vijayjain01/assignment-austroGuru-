f2f69a8398fabc8fa887e4344fec39f4488f93f7
vijayjain01 <98397527+vijayjain01@users.noreply.github.com>
2022-02-11 11:54:14 +0530

    Add files via upload

---
+++ assign austroGuru.js
@@ -0,0 +1,4 @@
+let austroGuru = (noOfChildren, spouceName, location, job)=>{
+    console.log( `You will be a ${job} in ${location}, and married to ${spouceName} with ${noOfChildren} kids.`);
+    }
+    austroGuru(2, "spouceName", "india", "student");
